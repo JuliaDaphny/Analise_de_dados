066561aba4049d5eadb68d0bb75caf47d19716d0
Júlia Daphny <61297824+JuliaDaphny@users.noreply.github.com>
2024-10-02 17:41:18 -0300

    Add files via upload

---
+++ 2021/Nanismo/script_nanismo_INSS_inss_mantidos_cessados.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE FUNCTION search_columns(
+    needle text,
+    haystack_tables name[] default '{}',
+    haystack_schema name[] default '{}'
+)
+RETURNS table(schemaname text, tablename text, columnname text, rowctid text)
+AS $$
+begin
+  FOR schemaname,tablename,columnname IN
+      SELECT c.table_schema,c.table_name,c.column_name
+      FROM information_schema.columns c
+        JOIN information_schema.tables t ON
+          (t.table_name=c.table_name AND t.table_schema=c.table_schema)
+        JOIN information_schema.table_privileges p ON
+          (t.table_name=p.table_name AND t.table_schema=p.table_schema
+              AND p.privilege_type='SELECT')
+        JOIN information_schema.schemata s ON
+          (s.schema_name=t.table_schema)
+      WHERE (c.table_name=ANY(haystack_tables) OR haystack_tables='{}')
+        AND (c.table_schema=ANY(haystack_schema) OR haystack_schema='{}')
+        AND t.table_type='BASE TABLE'
+  LOOP
+    FOR rowctid IN
+      EXECUTE format('SELECT ctid FROM %I.%I WHERE cast(%I as text) LIKE %L',
+       schemaname,
+       tablename,
+       columnname,
+       needle
+      )
+    LOOP
+      -- uncomment next line to get some progress report
+      RAISE NOTICE 'hit in %.%', schemaname, tablename;
+      RETURN NEXT;
+    END LOOP;
+ END LOOP;
+END;
+$$ language plpgsql;
+
+
+-- A busca diferencia maiusculo de minusculo
+-- CID nanismo = Q771 e E343(E343)
+SELECT count(*) FROM search_columns('E343',array(select table_name::name from information_schema.tables where table_name like 'tb_mantidos_ativos_2020%'), array['inss_mantidos_ativos']);
+
+--select * from inss_concedidos.tb_concedidos_201812 tc where cid like 'E34%'
+
